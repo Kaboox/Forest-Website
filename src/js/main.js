9efa87a21166a8fad1c9c039e1825d7dd1f3ab90
Kacper Smietana <kabox9@gmail.com>
2023-04-25 15:43:05 +0200

    disabled scroll while having mobile nav on

--- src/js/main.js
+++ src/js/main.js
@@ -3,19 +3,23 @@ const closeMenuBtn = document.querySelector('.close-menu')
 const mobileMenu = document.querySelector('.mobile-menu')
 const allMenuLinks = mobileMenu.querySelectorAll('a')
 const currentYear = document.querySelector('.year')
+const body = document.querySelector('body')
 
 currentYear.textContent = new Date().getFullYear();
 
 burgerBtn.addEventListener('click', () => {
     mobileMenu.classList.add('mobile-menu--active')
+    body.style.overflowY = 'hidden'
 })
 
 closeMenuBtn.addEventListener('click', () => {
     mobileMenu.classList.remove('mobile-menu--active')
+    body.style.overflowY = 'auto'
 })
 
 allMenuLinks.forEach(item => {
     item.addEventListener('click', () => {
         mobileMenu.classList.remove('mobile-menu--active')
+        body.style.overflowY = 'auto'
     })
 })
