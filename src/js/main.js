a98c95f109c8c339b5cf6e8751fb897e1b3a0315
Kacper Smietana <kabox9@gmail.com>
2023-04-25 14:50:39 +0200

    completed demo of main page

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,9 @@ const burgerBtn = document.querySelector('.burger')
 const closeMenuBtn = document.querySelector('.close-menu')
 const mobileMenu = document.querySelector('.mobile-menu')
 const allMenuLinks = mobileMenu.querySelectorAll('a')
+const currentYear = document.querySelector('.year')
+
+currentYear.textContent = new Date().getFullYear();
 
 burgerBtn.addEventListener('click', () => {
     mobileMenu.classList.add('mobile-menu--active')
