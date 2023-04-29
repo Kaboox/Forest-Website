3864043f17411d04fbafb92a0011144509c10b75
Kacper Smietana <kabox9@gmail.com>
2023-04-29 15:42:57 +0200

    reset

--- src/js/main.js
+++ src/js/main.js
@@ -9,18 +9,14 @@ const body = document.querySelector('body')
 currentYear.textContent = new Date().getFullYear();
 
 burgerBtn.addEventListener('click', () => {
-    mobileMenu.classList.toggle('mobile-menu--active')
-    if (mobileMenu.classList.contains('mobile-menu--active')) {
-        body.style.overflowY = 'hidden'
-    } else {
-        body.style.overflowY = 'auto'
-    }
+    mobileMenu.classList.add('mobile-menu--active')
+    body.style.overflowY = 'hidden';
 })
 
-// closeMenuBtn.addEventListener('click', () => {
-//     mobileMenu.classList.remove('mobile-menu--active')
-//     body.style.overflowY = 'auto'
-// })
+closeMenuBtn.addEventListener('click', () => {
+    mobileMenu.classList.remove('mobile-menu--active')
+    body.style.overflowY = 'auto'
+})
 
 allMenuLinks.forEach(item => {
     item.addEventListener('click', () => {
