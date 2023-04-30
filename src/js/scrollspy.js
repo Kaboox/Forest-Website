37e7fcfa75595147207c4637ae984a10a4a8d968
Kacper Smietana <kabox9@gmail.com>
2023-04-30 19:51:56 +0200

    repaired scrollspy

--- src/js/scrollspy.js
+++ src/js/scrollspy.js
@@ -1,18 +1,30 @@
-const navLinks = document.querySelectorAll('.nav__link');
+const navLinks = document.querySelectorAll('.nav__link ');
+const homeSection = document.querySelector('#home')
+const aboutusSection = document.querySelector('#about-us')
+const offerSection = document.querySelector('#offer')
 
+const section = []
+section.push(homeSection)
+section.push(aboutusSection)
+section.push(offerSection)
 
 document.addEventListener('scroll', () => {
-    if (window.scrollY >= 300 && window.scrollY < 1100) {
-        navLinks[1].classList.add('nav__link--present')
-        navLinks[2].classList.remove('nav__link--present')
-        navLinks[0].classList.remove('nav__link--present')
-    } else if (window.scrollY >= 1100) {
-        navLinks[1].classList.remove('nav__link--present')
-        navLinks[2].classList.add('nav__link--present')
-        navLinks[0].classList.remove('nav__link--present')
-    } else {
-        navLinks[0].classList.add('nav__link--present')
-        navLinks[1].classList.remove('nav__link--present')
-        navLinks[2].classList.remove('nav__link--present')
-    }
+    section.forEach(sec => {
+
+        let top = window.scrollY;
+        let offset = sec.offsetTop -150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('nav__link--present')
+                document.querySelector('.nav__link a[href*=' + id + ']').parentElement.classList.add('nav__link--present')
+            })
+        }
+
+
+    })
 })
+
+
