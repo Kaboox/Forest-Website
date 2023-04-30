f24b133a7a4c48f3c9280bb9c24ced0b306d69ad
Kacper Smietana <kabox9@gmail.com>
2023-04-30 15:20:27 +0200

    changed form validation to non else if statement

--- src/js/formValidator.js
+++ src/js/formValidator.js
@@ -1,44 +1,40 @@
 const nameInput = document.querySelector(".name");
 const mailInput = document.querySelector(".email");
-const mailError = document.querySelector('.email-error')
+const mailError = document.querySelector(".email-error");
 const textarea = document.querySelector(".message");
 const submitBtn = document.querySelector(".submit");
 let errCount = 0;
 
-const regex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
+const regex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
 
 const checkInput = (input) => {
 	const box = input.closest("div");
 	const error = box.querySelector(".form__error");
+	error.style.display = "none";
+	mailError.style.display = "none";
+	
 	if (input.value == "") {
 		error.style.display = "inline";
-        errCount++;
-	} else {
-		error.style.display = "none";
+		errCount++;
 	}
 
-	if (!(mailInput.value.match(regex))) {
+	if (!mailInput.value.match(regex)) {
 		errCount++;
-		mailError.style.display = 'inline'
-	} else {
-		mailError.style.display = 'none'
+		mailError.style.display = "inline";
 	}
 };
 
 submitBtn.addEventListener("click", (e) => {
 	errCount = 0;
-	e.preventDefault()
+	e.preventDefault();
 	checkInput(nameInput);
 	checkInput(mailInput);
 	checkInput(textarea);
 
 	if (errCount == 0) {
-		alert('Wysłano formularz')
-		nameInput.value = '';
-		mailInput.value = '';
-		textarea.value = '';
+		alert("Wysłano formularz");
+		nameInput.value = "";
+		mailInput.value = "";
+		textarea.value = "";
 	}
-
-    
-
 });
